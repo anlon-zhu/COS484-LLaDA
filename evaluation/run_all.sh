fc4fa7b7e424e68494dfe6000632b400f52af14c
Anlon Zhu <zhu.anlon@gmail.com>
2025-05-05 02:39:13 -0400

    add all

---
+++ evaluation/run_all.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+sbatch eval_mbpp.sbatch
+sbatch eval_mbpp_3shot.sbatch
+sbatch eval_humaneval.sbatch
+sbatch eval_humaneval_3shot.sbatch
+
+# === CodeGLUE ===
+sbatch eval_codexglue_python.sbatch
+sbatch eval_codexglue_java.sbatch
+sbatch eval_codexglue_javascript.sbatch
+sbatch eval_codexglue_go.sbatch
